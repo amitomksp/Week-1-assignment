375a92a295feb85832f84e02acae948dfbca82fe
Amit om kashyap <112419542+amitomksp@users.noreply.github.com>
2023-10-29 01:05:15 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,9 +6,28 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
-
+//javascript solution
 function isAnagram(str1, str2) {
 
+        // Get lengths of both strings
+        let n1 = str1.length;
+        let n2 = str2.length;
+   
+        // If length of both strings is not same,
+        // then they cannot be anagram
+        if (n1 != n2)
+            return false;
+   
+        // Sort both strings
+        str1.sort();
+        str2.sort()
+   
+        // Compare sorted strings
+        for (let i = 0; i < n1; i++)
+            if (str1[i] != str2[i])
+                return false;
+   
+        return true;
 }
 
 module.exports = isAnagram;
